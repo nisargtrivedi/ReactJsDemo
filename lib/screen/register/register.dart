53085743b891fbc50feddcfafbb2c0c3e87bf6fd
nisarg <nisarg.trivedi@vovance.com>
2020-11-10 11:42:00 +0530

    update code

---
+++ lib/screen/register/register.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Register'),
+    );
+  }
+}
---
+++ lib/screen/wrapper.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+//import 'package:myapp/screen/home/home.dart';
+import 'package:myapp/screen/register/register.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    //
+    return Register(); 
+  }
+}
